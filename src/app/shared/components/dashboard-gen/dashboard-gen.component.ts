dd25eb61707a3159397f42952f5a57bba808299f
dmysorenagaraj <dmysorenagaraj@hubgroup.com>
2019-11-24 02:19:50 -0600

    dashboard gen - need general component

---
+++ src/app/shared/components/dashboard-gen/dashboard-gen.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-dashboard-gen',
+  templateUrl: './dashboard-gen.component.html',
+  styleUrls: ['./dashboard-gen.component.scss']
+})
+export class DashboardGenComponent implements OnInit {
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    series: [{
+      data: [1, 2, 3],
+      type: 'line'
+    }]
+  };
+
+  // @Input() stockDashBoard: StockChart;
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 },
+          // { title: 'Card 2', cols: 1, rows: 1 },
+          // { title: 'Card 3', cols: 1, rows: 1 },
+          // { title: 'Card 4', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'Card 1', cols: 2, rows: 1 },
+        // { title: 'Card 2', cols: 1, rows: 1 },
+        // { title: 'Card 3', cols: 1, rows: 2 },
+        // { title: 'Card 4', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    // this.card3Data = stockDashBoard
+    // console.log(this.stockDashBoard);
+  }
+
+}
