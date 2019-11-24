51931d74b9bc682d04f37eb73e23ae6ea5af749b
dmysorenagaraj <dmysorenagaraj@hubgroup.com>
2019-11-24 02:19:23 -0600

    shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { DashboardGenComponent } from './components/dashboard-gen/dashboard-gen.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+import * as Highcharts from 'highcharts';
+
+
+@NgModule({
+  declarations: [
+    MainNavComponent,
+    DashboardGenComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    LayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    HighchartsChartModule
+  ],
+  exports: [
+    MainNavComponent,
+    DashboardGenComponent
+  ],
+  providers: [
+  ]
+})
+export class SharedModule { }
